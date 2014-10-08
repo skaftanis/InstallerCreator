f05e1134d44868f281947493722ec523a91d3b1b
skaftanis <skaftanis@gmail.com>
2014-10-08 17:02:12 +0300

    new GUI and right click installation staff

--- src/ui_mainwindow.h
+++ src/ui_mainwindow.h
@@ -31,6 +31,7 @@ public:
     QPushButton *pushButton_3;
     QLabel *label_2;
     QLabel *label_3;
+    QPushButton *pushButton_1;
     QMenuBar *menuBar;
     QStatusBar *statusBar;
 
@@ -38,15 +39,15 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(531, 248);
+        MainWindow->resize(531, 275);
         centralWidget = new QWidget(MainWindow);
         centralWidget->setObjectName(QString::fromUtf8("centralWidget"));
         pushButton_2 = new QPushButton(centralWidget);
         pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setGeometry(QRect(170, 90, 181, 41));
+        pushButton_2->setGeometry(QRect(170, 120, 181, 41));
         pushButton_3 = new QPushButton(centralWidget);
         pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-        pushButton_3->setGeometry(QRect(170, 140, 181, 41));
+        pushButton_3->setGeometry(QRect(170, 160, 181, 41));
         label_2 = new QLabel(centralWidget);
         label_2->setObjectName(QString::fromUtf8("label_2"));
         label_2->setGeometry(QRect(140, 40, 251, 41));
@@ -56,7 +57,15 @@ public:
         label_2->setFont(font);
         label_3 = new QLabel(centralWidget);
         label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setGeometry(QRect(320, 180, 181, 21));
+        label_3->setGeometry(QRect(190, 80, 131, 16));
+        pushButton_1 = new QPushButton(centralWidget);
+        pushButton_1->setObjectName(QString::fromUtf8("pushButton_1"));
+        pushButton_1->setGeometry(QRect(110, 210, 311, 22));
+        QFont font1;
+        font1.setFamily(QString::fromUtf8("Comfortaa"));
+        font1.setBold(false);
+        font1.setWeight(50);
+        pushButton_1->setFont(font1);
         MainWindow->setCentralWidget(centralWidget);
         menuBar = new QMenuBar(MainWindow);
         menuBar->setObjectName(QString::fromUtf8("menuBar"));
@@ -78,6 +87,7 @@ public:
         pushButton_3->setText(QApplication::translate("MainWindow", "Uninstall", 0, QApplication::UnicodeUTF8));
         label_2->setText(QApplication::translate("MainWindow", "InstallerCreator", 0, QApplication::UnicodeUTF8));
         label_3->setText(QApplication::translate("MainWindow", "by ShowMeYourCode", 0, QApplication::UnicodeUTF8));
+        pushButton_1->setText(QApplication::translate("MainWindow", "Install InstallerCreator for Nautilus Right Click", 0, QApplication::UnicodeUTF8));
     } // retranslateUi
 
 };
