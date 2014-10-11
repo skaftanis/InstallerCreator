3eb320d4c92ce188b844ba022d1422f1527d3541
skaftanis <skaftanis@gmail.com>
2014-10-11 21:41:45 +0300

    add uninstall choice in right click

--- Nautilus_Install_Setup.sh
+++ Nautilus_Install_Setup.sh
@@ -4,6 +4,9 @@
 cp src/"Install It" ~/.local/share/nautilus/scripts/
 chmod +x ~/.local/share/nautilus/scripts/"Install It"
 
+cp src/"Uninstall" ~/.local/share/nautilus/scripts/
+chmod +x ~/.local/share/nautilus/scripts/"Uninstall"
+
 zenity --question --title="InstallerCretor" --text="Done! Restart Nautilus Now?"
 if [ $? = 0 ]; then
 	killall nautilus
